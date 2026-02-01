91b9000a1a819a09799d8d991f2236053541f152
ChakornK <chakornk2007@gmail.com>
2026-01-31 22:06:58 -0800

    feat: save courses to db

--- src/app/api/courses/route.js
+++ src/app/api/courses/route.js
@@ -1,27 +1,9 @@
-import { cacheData } from "@/lib/redis";
+import { getCourses } from "@/lib/courses";
 import { NextResponse } from "next/server";
-
-const allowedDepts = ["CMPT", "MATH", "MACM", "STAT", "ENSC"];
 
 export async function GET(req, res) {
   try {
-    const data = await cacheData(
-      `courses`,
-      async () => {
-        const listRes = await fetch("https://api.sfucourses.com/v1/rest/outlines");
-        const courseList = await listRes.json();
-        return courseList.reduce((prev, { dept, number, title, description }) => {
-          if (allowedDepts.includes(dept)) {
-            prev.push({
-              code: `${dept} ${number}`,
-              title,
-            });
-          }
-          return prev;
-        }, []);
-      },
-      60 * 60 * 24 * 7,
-    );
+    const data = await getCourses();
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
