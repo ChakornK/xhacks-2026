cea6a2ea6f25ba24903d33d57d1581c033ace91d
ChakornK <chakornk2007@gmail.com>
2026-02-01 10:01:47 -0800

    feat: favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,6 +10,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="overflow-clip">
       <head>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="SFU CareerConnect" />
+        <link rel="manifest" href="/site.webmanifest" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
