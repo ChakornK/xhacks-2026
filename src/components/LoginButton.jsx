50bbcef67b203240f1b02ce2cbe18780c4b80de3
ChakornK <chakornk2007@gmail.com>
2026-02-01 07:22:14 -0800

    fix: prevent landing page button from showing loading state

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,41 +1,28 @@
 "use client";
 import { useSession, signIn } from "@/lib/auth-client";
+import { ChevronRightRounded } from "@mui/icons-material";
+import { Icon } from "@mui/material";
 import { useRouter } from "next/navigation";
 
 export default function SignInButton() {
-  // Check if the user is logged in
-  const { data: session, isPending } = useSession();
+  const { data: sessionData } = useSession();
   const router = useRouter();
 
-  if (isPending) {
-    return (
-      <button className="bg-sfu-red min-w-50 flex h-14 cursor-pointer items-center justify-center rounded px-8 text-2xl font-bold text-white shadow-lg transition-all hover:bg-[#8B1526]">
-        Loading...
-      </button>
-    );
-  }
-
-  if (session) {
-    return (
-      <button
-        className="bg-sfu-red min-w-50 flex h-14 items-center justify-center rounded px-8 text-lg font-bold text-white shadow-lg transition-all hover:scale-110 hover:bg-[#8B1526] active:scale-105"
-        onClick={() => router.push("/courses")}
-      >
-        Go to Courses
-      </button>
-    );
-  }
   return (
     <button
       onClick={async () => {
-        await signIn.social({
-          provider: "google",
-          callbackURL: "/courses",
-        });
+        if (sessionData?.session) {
+          router.push("/courses");
+        } else {
+          await signIn.social({
+            provider: "google",
+            callbackURL: "/courses",
+          });
+        }
       }}
-      className="bg-sfu-red min-w-50 flex h-14 cursor-pointer items-center justify-center rounded px-8 text-2xl font-bold text-white shadow-lg transition-all hover:bg-[#8B1526]"
+      className="btn btn-lg"
     >
-      Get Started
+      {sessionData?.session ? "Go to Courses" : "Get Started"} <Icon component={ChevronRightRounded} />
     </button>
   );
 }
